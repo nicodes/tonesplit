ccf772bcf9c510c50ae98afb77370a8b0554504c
Nico Zessoules <nicozessoules@gmail.com>
2024-08-17 18:24:43 -0600

    add second canvas

--- src/components/audioInterface/AudioInterface.svelte
+++ src/components/audioInterface/AudioInterface.svelte
@@ -12,8 +12,10 @@
 
   let audioContext: AudioContext;
   let tones: Tone[] = [createTone(), createTone(444)];
-  let canvas: HTMLCanvasElement;
-  let canvasContext: CanvasRenderingContext2D;
+  let canvasLeft: HTMLCanvasElement;
+  let canvasRight: HTMLCanvasElement;
+  let canvasContextLeft: CanvasRenderingContext2D;
+  let canvasContextRight: CanvasRenderingContext2D;
   let playing = false;
   const selectedChordDefault = "-- chord --";
   let selectedChord: typeof selectedChordDefault | keyof typeof chords =
@@ -31,7 +33,7 @@
   }
 
   function removeAllTones() {
-    tones.forEach((t) => stopTone(t));
+    tones.forEach(stopTone);
     tones = [];
   }
 
@@ -42,53 +44,65 @@
   }
 
   function toggleMute(index: number) {
-    if (tones[index].muted) startTone(audioContext, tones[index]);
-    else stopTone(tones[index]);
-    tones[index].muted = !tones[index].muted;
+    const tone = tones[index];
+    tone.muted = !tone.muted;
+    tone.muted ? stopTone(tone) : startTone(audioContext, tone);
   }
 
   function playChord(chord: keyof typeof chords) {
-    tones.forEach((t) => stopTone(t));
-    tones = [...tones, ...chords[chord].map((note) => createTone(note))];
+    tones.forEach(stopTone);
+    tones = [...tones, ...chords[chord].map(createTone)];
     if (playing) tones.forEach((t) => startTone(audioContext, t));
+  }
+
+  function drawChannel(
+    context: CanvasRenderingContext2D,
+    channel: HTMLCanvasElement,
+    sliceWidth: number,
+    tone: Tone,
+    index: number
+  ) {
+    context.lineWidth = 2;
+    context.strokeStyle = `rgb(${index * 60}, 0, 255)`;
+    context.beginPath();
+
+    let x = 0;
+    for (let i = 0; i < tone.bufferLength; i++) {
+      const v = tone.dataArray[i] / 128.0;
+      if (Math.abs(v) < 0.01) return;
+
+      const y = (v * channel.height) / 2;
+      i === 0 ? context.moveTo(x, y) : context.lineTo(x, y);
+      x += sliceWidth;
+    }
+    context.lineTo(channel.width, channel.height / 2);
+    context.stroke();
   }
 
   function drawWaveform() {
     if (!playing) return;
 
     requestAnimationFrame(drawWaveform);
-    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-    canvasContext.fillStyle = "rgb(200, 200, 200)";
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    canvasContextLeft.clearRect(0, 0, canvasLeft.width, canvasLeft.height);
+    canvasContextRight.clearRect(0, 0, canvasRight.width, canvasRight.height);
 
     tones.forEach((tone, index) => {
       if (tone.muted) return;
+
       tone.analyser.getByteTimeDomainData(tone.dataArray);
-      canvasContext.lineWidth = 2;
-      canvasContext.strokeStyle = `rgb(${index * 60}, 0, 255)`;
-      canvasContext.beginPath();
+      const sliceWidth = (canvasLeft.width * 6.0) / tone.bufferLength;
 
-      let x = 0;
-      let sliceWidth = (canvas.width * 6.0) / tone.bufferLength;
-      for (let i = 0; i < tone.bufferLength; i++) {
-        let v = tone.dataArray[i] / 128.0;
-        if (Math.abs(v) < 0.01) return;
-
-        let y = (v * canvas.height) / 2;
-        if (i === 0) canvasContext.moveTo(x, y);
-        else canvasContext.lineTo(x, y);
-        x += sliceWidth;
-      }
-
-      canvasContext.lineTo(canvas.width, canvas.height / 2);
-      canvasContext.stroke();
+      if (tone.pan >= 0)
+        drawChannel(canvasContextLeft, canvasLeft, sliceWidth, tone, index);
+      if (tone.pan <= 0)
+        drawChannel(canvasContextRight, canvasRight, sliceWidth, tone, index);
     });
   }
 
   onMount(() => {
-    audioContext = new ((window as any).AudioContext ||
-      (window as any).webkitAudioContext)();
-    canvasContext = canvas.getContext("2d")!;
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    canvasContextLeft = canvasLeft.getContext("2d")!;
+    canvasContextRight = canvasRight.getContext("2d")!;
   });
 
   $: if (playing) {
@@ -96,7 +110,7 @@
       t.osc.frequency.value = t.frequency;
       t.osc.type = t.oscType;
       t.panner.pan.value = t.pan;
-      t.gain.gain.value = t.muted ? 0 : t.volume; // Adjust gain based on muted state
+      t.gain.gain.value = t.muted ? 0 : t.volume;
     });
     drawWaveform();
   }
@@ -105,7 +119,9 @@
 <div class={styles.pedal}>
   <p class={styles.span}>ToneSplit</p>
 
-  <canvas bind:this={canvas} width="300" height="80" class={styles.span}
+  <canvas bind:this={canvasLeft} width="300" height="80" class={styles.span}
+  ></canvas>
+  <canvas bind:this={canvasRight} width="300" height="80" class={styles.span}
   ></canvas>
 
   <div class={styles.grid}>
@@ -143,7 +159,7 @@
     >
       ➕
     </button>
-    <select bind:value={selectedChord} id="exampleSelect" name="options">
+    <select bind:value={selectedChord}>
       <option value={selectedChordDefault}>{selectedChordDefault}</option>
       {#each Object.keys(chords) as c}
         <option value={c}>{c}</option>
