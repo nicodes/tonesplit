56531838db127dc8e4c86eb0bddacb339f410e1f
Nico Zessoules <nicozessoules@gmail.com>
2024-08-24 00:08:13 -0600

    setup pwa config

---
+++ pwa-config.ts
@@ -0,0 +1,23 @@
+import {
+    AssetType,
+    defaultAssetName,
+    defineConfig,
+    minimal2023Preset as preset,
+    ResolvedAssetSize
+  } from '@vite-pwa/assets-generator/config'
+  
+const inputImage = 'src/lib/images/svelte-logo.svg'
+const outputDir = '../../../static/'
+
+  export default defineConfig({
+    headLinkOptions: {
+      preset: '2023'
+    },
+    preset: {
+        ...preset,
+        assetName: (type: AssetType, size: ResolvedAssetSize) => {
+            return `${outputDir}${defaultAssetName(type, size)}`;
+          }
+    },
+    images: [inputImage],
+  })
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script>
+	import { pwaInfo } from 'virtual:pwa-info';
 	import '@fontsource/righteous';
 	import '../app.css';
+
+	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
+
+<svelte:head>
+	{@html webManifest}
+</svelte:head>
 
 <div class="app">
 	<!-- <Header /> -->
